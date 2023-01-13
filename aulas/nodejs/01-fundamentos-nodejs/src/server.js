227aea3b84d9e3868c43772cb51275b6deaa913a
consultores.brtech <consultores.brtech@mcbauchemie.com>
2023-01-13 20:26:49 -0300

    node watch mais script , npm  run dev , config package json

--- aulas/nodejs/01-fundamentos-nodejs/src/server.js
+++ aulas/nodejs/01-fundamentos-nodejs/src/server.js
@@ -3,7 +3,7 @@ import  http from 'node:http' //para import, mudar tag type no package
 
 const server = http.createServer((req, res)=> {
 
-  return res.end('hello')
+  return res.end('hellos')
 
 })
 
