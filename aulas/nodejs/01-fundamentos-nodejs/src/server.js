a31893034c170c5f5463617c40b5cf5c38eaaee9
consultores.brtech <consultores.brtech@mcbauchemie.com>
2023-01-13 20:39:40 -0300

    methodos diferentes, req, res, method  e url

--- aulas/nodejs/01-fundamentos-nodejs/src/server.js
+++ aulas/nodejs/01-fundamentos-nodejs/src/server.js
@@ -2,6 +2,15 @@ import  http from 'node:http' //para import, mudar tag type no package
 
 
 const server = http.createServer((req, res)=> {
+  const { method, url} = req
+
+  if ( method === 'GET' && url === '/users') {
+    return res.end('Listagem de usuários')
+  }
+
+  if ( method === 'POST' && url === '/users') {
+    return res.end('Criar usuários')
+  }
 
   return res.end('hellos')
 
