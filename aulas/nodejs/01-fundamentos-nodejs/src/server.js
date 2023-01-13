90233f41ff3e5d1052c52bb9c172f5ee8088a726
consultores.brtech <consultores.brtech@mcbauchemie.com>
2023-01-13 20:53:05 -0300

    adicionando headers, content type, JSON stringify

--- aulas/nodejs/01-fundamentos-nodejs/src/server.js
+++ aulas/nodejs/01-fundamentos-nodejs/src/server.js
@@ -1,14 +1,34 @@
 import  http from 'node:http' //para import, mudar tag type no package
 
+//Stateful - Stateles
+
+// Depende da mémoria / Não grava nada em memória 
+
+// Cabecalhos = Metadados, info adicional de como aquele dado por ser interpretado.
+
+const users = []
 
 const server = http.createServer((req, res)=> {
   const { method, url} = req
 
   if ( method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuários')
+    users.push({
+      id : 1,
+      name: 'John',
+      email: 'john@example.com'
+    })
+
+    return res.
+    setHeader('Content-Type', 'application/json')
+    .end(JSON.stringify(users))
   }
 
   if ( method === 'POST' && url === '/users') {
+    users.push({
+      id : 1,
+      name: 'John',
+      email: 'john@example.com'
+    })
     return res.end('Criar usuários')
   }
 
