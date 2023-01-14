56c5d46e8931fa0a0fd0b3ed591fd559b2ea9ab0
consultores.brtech <consultores.brtech@mcbauchemie.com>
2023-01-13 21:33:13 -0300

    Streams, readable, writable, transform

--- aulas/nodejs/01-fundamentos-nodejs/streams/fundamentos.js
+++ aulas/nodejs/01-fundamentos-nodejs/streams/fundamentos.js
@@ -4,7 +4,11 @@
 // conceito de receber o arquivo parcialmente e já processar / utilizar o mesmo.
 // dentro de streams não trabalho com números ou strings, ou seja , formatos primitivos
 
-import { Readable } from 'node:stream'
+// string de escrita não transforma nada
+
+// string de transformação, transforma um dado 
+
+import { Readable, Writable, Transform } from 'node:stream'
 
 class OneToHundredStream extends Readable {
 
@@ -26,4 +30,29 @@ class OneToHundredStream extends Readable {
   }
 }
 
-new OneToHundredStream().pipe(process.stdout)
+class MultiplyByTenStream extends Writable { 
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10)
+
+    callback()
+  }
+}
+
+class InverseNumberStream extends Transform { 
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1
+
+    callback(null, Buffer.from(String(transformed)))
+  }
+}
+
+
+// Lendo dados de uma stream, e escrevendo na Stream  que multiplica ( writable stream)
+//new OneToHundredStream().pipe(new MultiplyByTenStream())
+
+
+
+// Lendo dados de uma stream, transformado algo e escrevendo
+new OneToHundredStream()
+.pipe(new InverseNumberStream())
+.pipe(new MultiplyByTenStream())
