d32b46d98bbec9ebb6996031140df407418ce18f
consultores.brtech <consultores.brtech@mcbauchemie.com>
2023-01-20 16:09:01 -0300

    Criando o arquivo de rotas separado, colocando int. no server JS para achar a rota e path solicitado

--- aulas/nodejs/01-fundamentos-nodejs/src/server.js
+++ aulas/nodejs/01-fundamentos-nodejs/src/server.js
@@ -1,7 +1,6 @@
 import http from 'node:http' //para import, mudar tag type no package
 import { json } from './middlewares/json.js'
-import { Database } from './database.js'
-import { randomUUID } from 'node:crypto'
+import { routes } from './routes.js'
 
 //Stateful - Stateles
 
@@ -11,32 +10,23 @@ import { randomUUID } from 'node:crypto'
 
 // Http status code ( MDN )
 
-const database = new Database
+
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res)
 
-  if (method === 'GET' && url === '/users') {
-    const users = database.select('users')
-    return res.end(JSON.stringify(users))
+  const route = routes.find( route => {
+    return route.method === method && route.path === url 
+  })
+
+  // se encontrou uma rota, então executa a função padrão dela
+  if (route) {
+    return route.handler(req, res)
   }
 
-  if (method === 'POST' && url === '/users') {
-
-    const { name, email } = req.body
-
-    const user = {
-      id: randomUUID(),
-      name: name,
-      email: email
-    }
-
-    database.insert('users', user)
-
-    return res.writeHead(201).end()
-  }
+  console.log(route)
 
   return res.writeHead(404).end()
 
