e71ce505810dd164db591bcbdbc27e7ddeee2c85
consultores.brtech <consultores.brtech@mcbauchemie.com>
2023-01-27 18:22:45 -0300

    Testando components com propriedas

--- aulas/reactjs/aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/reactjs/aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,15 +1,16 @@
 import { Post } from "./Post";
 
-
 export function App() {
-
-
   return (
     <>
-    <Post/>
-
-
-    <Post/><Post/><Post/>
+      <Post
+        author="Bruno"
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit molestiae debitis voluptatem laudantium id, nulla excepturi blanditiis, magni nam quod velit nisi corrupti reiciendis consequuntur incidunt tempore doloribus amet aspernatur."
+      />
+      <Post
+        author="Bruno 2"
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit molestiae debitis voluptatem laudantium id, nulla excepturi blanditiis, magni nam quod velit nisi corrupti reiciendis consequuntur incidunt tempore doloribus amet aspernatur."
+      />
     </>
-  )
+  );
 }
