9bea93497cc053efe842def5157ea5b91b13fdcd
consultores.brtech <consultores.brtech@mcbauchemie.com>
2023-01-27 19:59:09 -0300

    Stilizando a sidebar, fazendo todo CSS necessário, trabalhando imgs, displays , overflow

--- aulas/reactjs/aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/reactjs/aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,19 +1,28 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
-import './global.css'
+import "./global.css";
+import styles from "./components/App.module.css";
+import { SideBar } from "./components/Sidebar";
 
 export function App() {
   return (
     <>
-      <Header/>
-      <Post
-        author="Bruno"
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit molestiae debitis voluptatem laudantium id, nulla excepturi blanditiis, magni nam quod velit nisi corrupti reiciendis consequuntur incidunt tempore doloribus amet aspernatur."
-      />
-      <Post
-        author="Bruno 2"
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit molestiae debitis voluptatem laudantium id, nulla excepturi blanditiis, magni nam quod velit nisi corrupti reiciendis consequuntur incidunt tempore doloribus amet aspernatur."
-      />
+      <Header />
+
+      <div className={styles.wrapper}>
+        <SideBar/>
+        <main>
+          
+          <Post
+            author="Bruno"
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit molestiae debitis voluptatem laudantium id, nulla excepturi blanditiis, magni nam quod velit nisi corrupti reiciendis consequuntur incidunt tempore doloribus amet aspernatur."
+          />
+          <Post
+            author="Bruno 2"
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit molestiae debitis voluptatem laudantium id, nulla excepturi blanditiis, magni nam quod velit nisi corrupti reiciendis consequuntur incidunt tempore doloribus amet aspernatur."
+          />
+        </main>
+      </div>
     </>
   );
 }
