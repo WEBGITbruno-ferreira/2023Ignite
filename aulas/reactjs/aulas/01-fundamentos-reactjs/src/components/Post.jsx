bb9a22093d8a53ba2907bfcbc4ade5439bdaf0a5
consultores.brtech <consultores.brtech@mcbauchemie.com>
2023-01-27 21:30:24 -0300

    Finalizando comentários, trick CSS para ocultar o botão .

--- aulas/reactjs/aulas/01-fundamentos-reactjs/src/components/Post.jsx
+++ aulas/reactjs/aulas/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,31 +1,56 @@
-import styles from './Post.module.css'
+import styles from "./Post.module.css";
 
-export function Post () {
+export function Post() {
   return (
-
-    <article className={styles.post}> 
+    <article className={styles.post}>
       <header>
-          <div className={styles.author}> 
-            <img className={styles.avatar} src="https://github.com/WEBGITbruno-ferreira.png"  alt="" />
-            <div className={styles.authorInfo}> 
-                <strong> Bruno Ferreira</strong>
-                <span> Web Dev</span>
-            </div>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/WEBGITbruno-ferreira.png"
+            alt=""
+          />
+          <div className={styles.authorInfo}>
+            <strong> Bruno Ferreira</strong>
+            <span> Web Dev</span>
           </div>
+        </div>
 
-          <time title="2022-05-11 as 8h:00m" dateTime='2022-05-11 08:00:00'> Públicado há 1h</time>
+        <time title="2022-05-11 as 8h:00m" dateTime="2022-05-11 08:00:00">
+          {" "}
+          Públicado há 1h
+        </time>
       </header>
 
       <div className={styles.content}>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-         <p> Sed error numquam rerum magnam iusto hic, tempore, voluptatem maxime sint nisi eius sit blanditiis porro esse deserunt mollitia eos id omnis?</p>
-         <p> <a href="https://github.com/WEBGITbruno-ferreira"> https://github.com/WEBGITbruno-ferreira </a> </p>
-         <p> <a href=""> #rocket </a> {' '}
-              <a href="">#teste </a> {' '}
-              <a href="">#teste </a>
-         </p>
+        <p>
+          {" "}
+          Sed error numquam rerum magnam iusto hic, tempore, voluptatem maxime
+          sint nisi eius sit blanditiis porro esse deserunt mollitia eos id
+          omnis?
+        </p>
+        <p>
+          {" "}
+          <a href="https://github.com/WEBGITbruno-ferreira">
+            {" "}
+            https://github.com/WEBGITbruno-ferreira{" "}
+          </a>{" "}
+        </p>
+        <p>
+          {" "}
+          <a href=""> #rocket </a> <a href="">#teste </a> <a href="">#teste </a>
+        </p>
       </div>
-    </article>
-  )
-}
 
+      <form className={styles.commentForm}>
+        {" "}
+        <strong> Deixe seu feedback</strong>
+        <textarea placeholder="deixe um comentário"/>
+        <footer> 
+        <button type="submit">  Publicar</button>
+        </footer>
+      </form>
+    </article>
+  );
+}
