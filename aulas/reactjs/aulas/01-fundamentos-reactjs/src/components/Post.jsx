d0f7ee5e8110b718905928a6e1f5d77de84848e4
consultores.brtech <consultores.brtech@mcbauchemie.com>
2023-01-28 16:31:45 -0300

    Separando avatar em componente, props,  desestruturando props recebidas no comp.

--- aulas/reactjs/aulas/01-fundamentos-reactjs/src/components/Post.jsx
+++ aulas/reactjs/aulas/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
@@ -6,11 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/WEBGITbruno-ferreira.png"
-            alt=""
-          />
+        <Avatar src="https://github.com/WEBGITbruno-ferreira.png"/> 
           <div className={styles.authorInfo}>
             <strong> Bruno Ferreira</strong>
             <span> Web Dev</span>
--- aulas/reactjs/aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ aulas/reactjs/aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import styles from './SideBar.module.css'
 import { PencilLine} from 'phosphor-react'
+import { Avatar } from './Avatar'
 
 export function SideBar() {
   return (
@@ -11,7 +12,7 @@ export function SideBar() {
     />
     
     <div className={styles.profile}> 
-      <img className={styles.avatar}src="https://github.com/WEBGITbruno-ferreira.png"/>  
+     <Avatar src="https://github.com/WEBGITbruno-ferreira.png"/> 
       <strong>Bruno F.</strong> 
       <span> Web Dev</span>
     </div>
