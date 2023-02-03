c37234bf126662e404b5854c5254147e17b54e2e
consultores.brtech <consultores.brtech@mcbauchemie.com>
2023-02-03 20:11:09 -0300

    arrow function direto sem criar nova function para onclick

--- aulas/reactjs/aulas/01-fundamentos-reactjs/src/components/Comment.jsx
+++ aulas/reactjs/aulas/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,4 +1,5 @@
 import { ThumbsUp, Trash } from "phosphor-react";
+import { useState } from "react";
 import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
 
@@ -6,10 +7,19 @@ import styles from "./Comment.module.css";
 
 export function Comment({content, onDeleteComment}) {
 
+const [likeCount, setLikeCount] = useState (0)
+
+
 function handleDeleteComment() {
 
   onDeleteComment(content)
 }
+
+/*
+function handleLikeComment(){
+  
+  setLikeCount(likeCount + 1)
+}*/
 
   return (
     <div className={styles.comment}>
@@ -34,9 +44,9 @@ function handleDeleteComment() {
         </div>
 
         <footer>
-          <button>
+          <button onClick={ () => setLikeCount(likeCount + 1)}>
             <ThumbsUp size={20} />
-            Aplaudir <span> 20 </span>
+            Aplaudir <span> {likeCount} </span>
           </button>
         </footer>
       </div>
