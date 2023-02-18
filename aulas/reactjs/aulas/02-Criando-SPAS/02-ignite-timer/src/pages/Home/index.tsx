596e2bb4ee3f93edd366eea85a6f43b48a26450f
Bruno Ferreira <brunoalmeidaferreira@hotmail.com>
2023-02-18 13:22:16 -0300

    ZOD VALID E INTERFACE com TYPEOF

--- aulas/reactjs/aulas/02-Criando-SPAS/02-ignite-timer/src/pages/Home/index.tsx
+++ aulas/reactjs/aulas/02-Criando-SPAS/02-ignite-timer/src/pages/Home/index.tsx
@@ -16,27 +16,46 @@ import {
 
 const newCylcleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
-  minutesAmount: zod.number().min(5).max(60),
+  minutesAmount: zod
+    .number()
+    .min(5)
+    .max(60, 'O ciclo deve ser no máximo 60 minutos'),
 })
 // formulario controlados ou  uncontrolled
 // controlados, armazenando os valores dos input no estado, monitorando as vars em tempo real / Pode ser um problema para renderização do react, que sempre  que alteradmos um estado o react renderiza
+
+// interface NewCycleFormData {
+//  task: string
+//  minutesAmount: number
+// }
+// funcionalidades do TS, não enxergam variaveis do JS, portanto uso o TYPEOF ao criar essa nova interface
+// baseada no schema criado pelo ZOD anteriormente.
+
+// A principal vantagem de criar este estilo de interface, é que ao criar os valids do ZOD no schema, ele já reconhece as
+// variaveis aqui no interface.
+type NewCycleFormData = zod.infer<typeof newCylcleFormValidationSchema> // criando com base em outra variavel
 
 // uncontrolled - Busca os valores dos inputs no submit do form, perdemos a fluidez e ganhamos  em desempenho
 export function Home() {
   // uso o register em cada input do form, e no onSubmit do form, coloco a funcao handleSubmit do userForm,
   // passando como parâmetro minha funcao
-  const { register, handleSubmit, watch, formState } = useForm({
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCylcleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
   })
 
-  function handleCreateNewCycle(data: any) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
   }
 
   const task = watch('task')
   const isSubmitDisabled = !task
 
-  console.log(formState.errors)
+  // return dos erros do formulário
+  // console.log(formState.errors)
 
   return (
     <HomeContainer>
